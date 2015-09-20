36001c37fc489752b1c020d9d9af517a33269c5a
k3t0 <orellgarten@gmx.de>
2015-09-20 11:26:44 +0200

    Readme + better usability

---
+++ FPGALoader/README.md
@@ -0,0 +1,1 @@
+Simple python script which allows user to chose the bitstream file to flash the FPGA with it. Makes use of xc3sprog.
--- FPGALoader/loader.py
+++ FPGALoader/loader.py
@@ -3,13 +3,14 @@ import tkinter.filedialog as fdialog
 
 if __name__ == "__main__":
     
-    #if not os.geteuid()==0:
-     #   sys.exit("\nYou must be root to run this application, please    use sudo and try again.\n")  
+    xpath = 'xc3sprog/xc3sprog';
+    ppath = '~/projects';
+    if not os.geteuid()==0:
+       sys.exit("\nYou must be root to run this application, please    use sudo and try again.\n")  
         
-    bitfile = fdialog.askopenfilename(filetypes = [('Bistream', '*.bit')], title="Open Bitstream", 
-                                                   initialdir=(os.path.expanduser('~/ElektronikProgrammierung/Elektronik/FPGA/projects')) )
+    bitfile = fdialog.askopenfilename(filetypes = [('Bistream', '*.bit')], title="Open Bitstream", initialdir=(os.path.expanduser(ppath) ) )
     print("loaded file: " + bitfile)
-    args = ['xc3sprog/xc3sprog', '-c', 'ftdi', '-v', bitfile]
+    args = [xpath, '-c', 'ftdi', '-v', bitfile]
     print("Command: " +  args[0] + " " + args[1] + " " +  args[2] + " " + args[3] + " " +  args[4])
     p = subprocess.Popen(args)    
     output, error = p.communicate()
